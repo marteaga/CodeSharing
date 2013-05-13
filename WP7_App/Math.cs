efcd5d07ab084becb15bcd1e6e5ff69b09b852bd
Mark Arteaga <info@markarteaga.com>
2013-05-12 20:38:46 -0400

    added the initial math class

---
+++ WP7_App/Math.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public class Math
+    {
+        private static Math _default;
+        public static Math Default
+        {
+            get
+            {
+                if (_default == null)
+                    _default = new Math();
+                return _default;
+            }
+        }
+
+        private Math()
+        {
+        }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiple(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+    }
+}
