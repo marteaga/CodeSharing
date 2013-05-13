4b0a46124e0945c3b8a79b31fed985557c64d217
Mark Arteaga <info@markarteaga.com>
2013-05-13 02:50:17 -0400

    added math2 PCL

---
+++ Math2/Math2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public partial class Math2
+    {
+        private static Math2 _default;
+        public static Math2 Default
+        {
+            get
+            {
+                if (_default == null)
+                    _default = new Math2();
+                return _default;
+            }
+        }
+
+        private Math2()
+        {
+        }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiple(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public float Divide(float x, float y)
+        {
+            return x / y;
+        }
+    }
+}
